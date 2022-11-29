52697263e95bbcac29802405704ef14306a8bf4e
haamid10 <apdixamiid919@gmail.com>
2022-11-29 21:57:38 +0300

    tuesday

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = ({bgColor,text,textColor}) => {
+  const headerStyles ={
+    backgroundColor: bgColor,
+    color: textColor
+     
+  }
+  return (
+    <header style={headerStyles}>
+    <div className='container'> 
+     <h1>{text}</h1> 
+    </div>
+    </header>
+  )
+}
+Header.defaultProps ={
+  text: 'Feedback UI',
+  bgColor: 'rgba(0,0,0,0.4)',
+  textColor: '#ff6a95'
+}
+export default Header
