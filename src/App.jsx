214deb5feccd4f50e068c9fca27cafb342a9dc28
haamid10 <apdixamiid919@gmail.com>
2022-11-30 19:59:59 +0300

    added feedback Stats

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+
 function App() {
   const [feedback, setFeedback]= useState(FeedbackData)
   const deleteFeedback = (id) =>{
@@ -13,6 +15,7 @@ function App() {
     < >
      <Header/>
      <div className="container">
+      <FeedbackForm/> 
       <FeedbackStats feedback={feedback}/>
       <FeedbackList feedback={feedback} onAdd={deleteFeedback}/>
       <h1>My App</h1>
---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,18 @@
+import Card from "./shared/Card"
+import {useState} from 'react'
+const FeedbackForm = () => {
+  const [text, setText] = useState('')
+  return (
+    <Card>
+      <form action="">
+        <h2>How would you rate your service with us?</h2>
+        <div className="input-group">
+          <input type="text " placeholder="write the review" value={text} onChange={(e)=>setText(e.target.value)} />
+          <button type="submit " >Send</button>
+        </div>
+      </form>
+      </Card>
+  )    
+}
+
+export default FeedbackForm
