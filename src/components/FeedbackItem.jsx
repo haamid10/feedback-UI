57cc033eb6a6dbef1ed3f1e4a7bf839b52528647
haamid10 <apdixamiid919@gmail.com>
2022-11-30 13:00:28 +0300

    added global components to wrap that make eaisier to style

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -2,7 +2,8 @@ import Card from "./shared/Card"
 const FeedbackItem = ({item}) => {
  
   return (
-    <Card>
+    <Card >
+       {/* <button onClick={reverse=true}></button> */}
     <div className="num-display">{item.rating}</div>
     <div className="text-display">{item.text}</div>
     {/* <button onClick={handleClick}>Click</button> */}
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,6 @@
 import FeedbackItem from "./FeedbackItem"
+import PropTypes from 'prop-types'
+
 const FeedbackList = ({feedback}) => {
   if(!feedback||feedback.length === 0) {
     return <p>No feedbacks Yet</p>
@@ -7,6 +9,15 @@ const FeedbackList = ({feedback}) => {
     <div className='feedback-list'>
      {feedback.map((item)=>(
       <FeedbackItem key={item.id} item={item}/>))}</div>
+  )
+}
+FeedbackList.PropTypes ={
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired
+    })
   )
 }
 
