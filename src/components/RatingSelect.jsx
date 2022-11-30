2de0ee45ae07ebc363727e81e44272555509df80
haamid10 <apdixamiid919@gmail.com>
2022-11-30 22:01:34 +0300

    added Rating select

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -67,21 +67,53 @@ const RatingSelect = () => {
       <input 
       type="radio" 
       name="rating"
-      id='num5'
-      value='5' 
+      id='num6'
+      value='6' 
       onChange={handleChange}
-       checked={selected==5} />
-    <label htmlFor='num5'>5</label>
+       checked={selected==6} />
+    <label htmlFor='num6'>6</label>
+
+    </li> 
+     <li>
+      <input 
+      type="radio" 
+      name="rating"
+      id='num7'
+      value='7' 
+      onChange={handleChange}
+       checked={selected==7} />
+    <label htmlFor='num7'>7</label>
+
+    </li>
+    <li>
+      <input 
+      type="radio" 
+      name="rating"
+      id='num8'
+      value='8' 
+      onChange={handleChange}
+       checked={selected==8} />
+    <label htmlFor='num8'>8</label>
 
     </li>  <li>
       <input 
       type="radio" 
       name="rating"
-      id='num1'
-      value='1' 
+      id='num9'
+      value='9' 
       onChange={handleChange}
-       checked={selected==1} />
-    <label htmlFor='num1'>1</label>
+       checked={selected==9} />
+    <label htmlFor='num9'>9</label>
+
+    </li>  <li>
+      <input 
+      type="radio" 
+      name="rating"
+      id='num10'
+      value='10' 
+      onChange={handleChange}
+       checked={selected==10} />
+    <label htmlFor='num10'>10</label>
 
     </li>
    
