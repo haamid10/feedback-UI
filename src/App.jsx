34c5fcfef0218cfa537bfb40abe297f9d575ad7f
haamid10 <apdixamiid919@gmail.com>
2022-11-30 10:56:49 +0300

    added useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
 function App() {
   return (
     < >
      <Header/>
      <div className="container">
+      <FeedbackItem/>
       <h1>My App</h1>
      </div>
      
