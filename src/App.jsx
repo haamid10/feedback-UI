d5e51bfa7a22e80127dcb7aa56393b52c4f6fcfd
haamid10 <apdixamiid919@gmail.com>
2022-12-01 11:29:10 +0300

    SUORE WDNESDAY added the functionality of the form

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,22 @@ import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
   const [feedback, setFeedback]= useState(FeedbackData)
+
   const deleteFeedback = (id) =>{
     if(window.confirm('Are you sure you went to Delete')){
    setFeedback(feedback.filter((item)=>item.id!==id))}
   }
+
+const addFeedback = (newFeedback)=>{
+  // const id= Math.floor(Math.random()*100)+1
+  // const newFeedback=[...feedback,id]
+  setFeedback([...feedback,newFeedback])
+}
   return (
     < >
      <Header/>
      <div className="container">
-      <FeedbackForm/> 
+      <FeedbackForm onadd={addFeedback}/> 
       <FeedbackStats feedback={feedback}/>
       <FeedbackList feedback={feedback} onAdd={deleteFeedback}/>
       <h1>My App</h1>
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,15 +3,16 @@ import RatingSelect from "./RatingSelect"
 import Button from "./Button"
 import {useState} from 'react'
 
-const FeedbackForm = () => {
+const FeedbackForm = ({onadd}) => {
   const [text, setText] = useState('')
   const [btnDisabled, setBtnDisabled] = useState(true)
+  const [rating, setRating] = useState(10)
   const [message, setMessage] = useState('')
   const handleTextChange = (e)=>{
     if(text === ''){
       setBtnDisabled(true)
       setMessage(null)}
-      else if(text !== ''&&text.trim().length<= 10){
+      else if(text !== ''&&text.trim(). length<= 10){
         setMessage('please enter more than characteristics')
 
         setBtnDisabled(true)
@@ -22,14 +23,26 @@ const FeedbackForm = () => {
     }
     setText(e.target.value)
   }
+  const handleSubmit =(e)=>{
+  e.preventDefault()
+  if(text.trim().length > 10){
+    const newFeedback ={
+      rating,
+      text,
+    }
+    
+    onadd(newFeedback)
+    setText('')
+  }
+  }
   return (
     <Card>
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
         <RatingSelect select ={(rating)=>setRating(rating)}/> 
         <div className="input-group">
           <input type="text " placeholder="write the review" value={text} onChange={handleTextChange} />
-          <Button type="submit"   isDisabled={btnDisabled}>Send</Button>
+          <Button type="submit"   isDisabled={btnDisabled} >Send</Button>
         </div >
         {message&&<div className="message ">{message}</div>}
       </form>  
