ef8b26cb399b5bc74f11da4ef7e778ef0e2a2412
haamid10 <apdixamiid919@gmail.com>
2022-12-05 14:32:13 +0300

    usecontext in FeedbackStats

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,6 +1,9 @@
 
-import PropTypes from 'prop-types'
-const FeedbackStats = ({feedback}) => {
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
+const FeedbackStats = () => {
+  const{feedback}=useContext(FeedbackContext)
+
   let average = feedback.reduce((acc,cur)=>{
     return acc + cur.rating
   },0)/feedback.length
@@ -15,8 +18,6 @@ const FeedbackStats = ({feedback}) => {
       </div>
   )
 }
-FeedbackStats.PropType ={
-  feedback : PropTypes.array.isRequired 
-}
+
 
 export default FeedbackStats 
