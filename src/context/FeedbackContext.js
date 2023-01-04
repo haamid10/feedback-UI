8b19eabd0b393ef4afaea6dcc53dafc37a7ea49b
haamid10 <apdixamiid919@gmail.com>
2023-01-04 20:55:39 +0300

    delete Activated

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -54,8 +54,9 @@ export const FeedbackProvider =({children})=>{
 
  }
   // delete feedback
-  const deleteFeedback = (id) =>{
+  const deleteFeedback = async (id) =>{
     if(window.confirm('Are you sure you went to Delete')){
+     await fetch(`/feedback/${id}`,{method: 'DELETE'})
    setFeedback(feedback.filter((item)=>item.id!==id))}
   }
   return <FeedbackContext.Provider 
